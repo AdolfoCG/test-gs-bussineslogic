c0fe5d47ddfc5a8cd8527123db1e92360aa79d2e
Adolfo Cordoba <adolfo.cordoba@coppel.com>
2025-02-03 12:07:24 -0600

    Inicialización del proyecto

---
+++ src/main/java/com/adolfo/test/gs/bussineslogic/BussineslogicApplication.java
@@ -0,0 +1,13 @@
+package com.adolfo.test.gs.bussineslogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussineslogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussineslogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adolfo/test/gs/bussineslogic/BussineslogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adolfo.test.gs.bussineslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussineslogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
